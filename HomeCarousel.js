88d8812f5efa88a74403ab158dcf89dae3b3ecd6
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-22 06:48:43 +0530

    Initial Commit for file: HomeCarousel.js

---
+++ HomeCarousel.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Image, View, FlatList, StyleSheet } from 'react-native'
+
+
+
+const homeCarouselItem = ({ item }) => (
+<View style={styles.home_carousel_item}>
+<Image
+    style={styles.art_photo}
+    source={{uri: item.art_photo}}
+    />
+</View>
+  );
+
+const HomeCarousel = ({ item }) => (
+<FlatList
+    horizontal={true}
+    style={styles.home_carousel}
+    data={item}
+    renderItem={homeCarouselItem}
+    keyExtractor={item => item.id}
+    showsHorizontalScrollIndicator={false}
+    pagingEnabled={true}
+    />
+);
+
+export default HomeCarousel;
+
+const styles = StyleSheet.create({
+    'art_photo': {
+        'width': '50vw',
+        'height': '100vw',
+        'marginTop': 5,
+        'backgroundColor': '#fafafaff',
+        'margin': 0,
+        'marginHorizontal': 5
+    }
+});
