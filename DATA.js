58da046b3d79547cfbdc6475b75977c61b091aa1
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-22 06:48:46 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "b5d0335f-9313-4c05-82af-ee861a4d094e",
+    "home_feed": [
+        {
+            "id": "c092be98-91fd-4f30-9130-da0e5d9cc609",
+            "artist_name": "want",
+            "home_carousel": [
+                {
+                    "id": "6406ec95-d927-4705-a47d-47c6acc0838c",
+                    "art_photo": "https://images.unsplash.com/photo-1530315812402-a8479a3ce902?w=1080"
+                },
+                {
+                    "id": "fffa2456-aa76-441a-8a42-76aa552b755f",
+                    "art_photo": "https://images.unsplash.com/photo-1642506539692-99e21cbd2f5c?w=1080"
+                },
+                {
+                    "id": "f0be20f0-c0da-4c1d-931e-728b931ffd68",
+                    "art_photo": "https://images.unsplash.com/photo-1609870918932-2088b105d12f?w=1080"
+                },
+                {
+                    "id": "0ceb66c9-7bc3-4db3-868a-5931ace79077",
+                    "art_photo": "https://images.unsplash.com/photo-1649508083072-4010347b47f8?w=1080"
+                },
+                {
+                    "id": "f807f1f1-671c-4e32-a282-492ef27c862c",
+                    "art_photo": "https://images.unsplash.com/photo-1581008139236-ebdede9f881d?w=1080"
+                }
+            ],
+            "art_caption": "international",
+            "art_description": "Training news minute least stand daughter above. Mrs example million walk well paper."
+        },
+        {
+            "id": "7ec7ca3a-7971-4496-bb44-53dab1145ce6",
+            "artist_name": "range",
+            "home_carousel": [
+                {
+                    "id": "d02b6163-d431-45bc-964f-58da67326ad6",
+                    "art_photo": "https://images.unsplash.com/photo-1585934302788-c141f024d86e?w=1080"
+                },
+                {
+                    "id": "73b7f349-be75-4d3d-b34a-21bcae35ed16",
+                    "art_photo": "https://images.unsplash.com/photo-1578917540773-7bdd2bbc13a8?w=1080"
+                },
+                {
+                    "id": "1101a395-aa77-422d-9ef8-4bf04250e365",
+                    "art_photo": "https://images.unsplash.com/photo-1523350165414-082d792c4bcc?w=1080"
+                },
+                {
+                    "id": "ac5c946b-d959-4ff7-bd39-8c0a53700a99",
+                    "art_photo": "https://images.unsplash.com/photo-1559333203-e5222606a9db?w=1080"
+                },
+                {
+                    "id": "d6f3233c-8d15-4305-9ce7-8ec0e9703e26",
+                    "art_photo": "https://images.unsplash.com/photo-1551232864-dc859a0b6de2?w=1080"
+                }
+            ],
+            "art_caption": "evidence",
+            "art_description": "Hope color expect send half produce. Help subject seem road space. Performance management personal reason yourself."
+        },
+        {
+            "id": "2bc8157d-e4b8-420a-b584-26fae7d817fd",
+            "artist_name": "lay",
+            "home_carousel": [
+                {
+                    "id": "adcc7801-58c8-4e4d-97e1-022843617ac9",
+                    "art_photo": "https://images.unsplash.com/photo-1469281570944-8d8ce51a1749?w=1080"
+                },
+                {
+                    "id": "2e50bc3b-7915-41a7-8038-66bc6074fb23",
+                    "art_photo": "https://images.unsplash.com/photo-1536551739350-d473d0f5d66a?w=1080"
+                },
+                {
+                    "id": "e4949376-0f47-4ac4-b05d-d6032614780e",
+                    "art_photo": "https://images.unsplash.com/photo-1530948511201-3386d4730d73?w=1080"
+                },
+                {
+                    "id": "dce19416-a598-4621-8ba8-8f74db647d10",
+                    "art_photo": "https://images.unsplash.com/photo-1531346878377-a5be20888e57?w=1080"
+                },
+                {
+                    "id": "0747f80d-e941-4671-ba0f-f00c41febd5f",
+                    "art_photo": "https://images.unsplash.com/photo-1604411853851-8b4d5d910545?w=1080"
+                }
+            ],
+            "art_caption": "understand",
+            "art_description": "Issue admit arrive this establish kid spring. One skin age support possible since. Including them wait color."
+        },
+        {
+            "id": "e68304fc-fa52-4efb-9708-87429c7ff3ec",
+            "artist_name": "good",
+            "home_carousel": [
+                {
+                    "id": "4b515bdd-32d0-46e3-abf0-eadaa97bb65b",
+                    "art_photo": "https://images.unsplash.com/photo-1548438294-1ad5d5f4f063?w=1080"
+                },
+                {
+                    "id": "2b2b8625-7667-45da-bba1-621d9933e040",
+                    "art_photo": "https://images.unsplash.com/photo-1633408771159-e0916aae173a?w=1080"
+                },
+                {
+                    "id": "5872c4f6-1867-42e3-bebd-707779b9d52e",
+                    "art_photo": "https://images.unsplash.com/source-404?w=1080"
+                },
+                {
+                    "id": "fe6208aa-1e37-4d30-939e-32dbad35fc4f",
+                    "art_photo": "https://images.unsplash.com/photo-1576956173118-0d3827d55390?w=1080"
+                },
+                {
+                    "id": "545ac04e-9845-4aa7-9c76-a7c6fd1dab38",
+                    "art_photo": "https://images.unsplash.com/photo-1571261354542-7dd5fde6d461?w=1080"
+                }
+            ],
+            "art_caption": "that",
+            "art_description": "Democrat about large. Nice student its civil pretty environmental. Increase picture rather enough same some."
+        },
+        {
+            "id": "f43fd6b7-7f94-4dc7-a5f9-d583ee2679ef",
+            "artist_name": "treatment",
+            "home_carousel": [
+                {
+                    "id": "1b07e33b-b6fc-46f7-b769-0d85a8494bf5",
+                    "art_photo": "https://images.unsplash.com/photo-1603175688981-111822a93216?w=1080"
+                },
+                {
+                    "id": "0542bfc8-3fd8-42ac-8597-6d3ef09a798a",
+                    "art_photo": "https://images.unsplash.com/photo-1552831016-6386d221b8f9?w=1080"
+                },
+                {
+                    "id": "04067904-4ba5-4566-a335-f1029abd3190",
+                    "art_photo": "https://images.unsplash.com/photo-1560785477-d43d2b34e0df?w=1080"
+                },
+                {
+                    "id": "3fad976d-d038-4ca6-b7d0-ff30a4a110df",
+                    "art_photo": "https://images.unsplash.com/photo-1615068029474-c1963dca549a?w=1080"
+                },
+                {
+                    "id": "445df576-d0e6-439d-9ef8-59e29de73bf8",
+                    "art_photo": "https://images.unsplash.com/source-404?w=1080"
+                }
+            ],
+            "art_caption": "traditional",
+            "art_description": "Difficult rise fire produce vote modern. Common structure member direction financial send participant. Around result will charge future force yourself cold."
+        }
+    ]
+}
